9e593499cca278750dbd5cf070b819782182c489
dohyun-kor <dnen14@naver.com>
2024-10-07 22:06:46 +0900

    [Gold III] Title: 웜홀, Time: 656 ms, Memory: 26176 KB -BaekjoonHub

--- 백준/Gold/1865. 웜홀/웜홀.java
+++ 백준/Gold/1865. 웜홀/웜홀.java
@@ -4,6 +4,17 @@ import java.util.*;
 public class Main
 {
 	static int N,M,W;
+	static ArrayList<node> nodeList;
+	static String result;
+
+	static class node {
+		int x,y,cost;
+		node(int x, int y, int cost) {
+			this.x = x;
+			this.y = y;
+			this.cost = cost;
+		}
+	}
 
 	public static void main(String[] args)throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,51 +26,53 @@ public class Main
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
-			int floyd[][] = new int[N+1][N+1];
-			int INF = 10000 * M;
-			for(int i=0; i<N+1; i++) {
-				Arrays.fill(floyd[i],INF);
-				floyd[i][i] = 0;
-			}
+
+			nodeList = new ArrayList<>();
 
 			for(int i=0; i<M; i++) {
 				st = new StringTokenizer(br.readLine());
 				int start = Integer.parseInt(st.nextToken());
 				int end = Integer.parseInt(st.nextToken());
 				int cost = Integer.parseInt(st.nextToken());
-				if(floyd[start][end] > cost) {
-					floyd[start][end] = cost;
-				}
-				if(floyd[end][start] > cost){
-				    floyd[end][start] = cost;
-				}
+				nodeList.add(new node(start,end,cost));
+				nodeList.add(new node(end,start,cost));
 			}
-			
-			for(int i=0; i<W; i++){
-			    st = new StringTokenizer(br.readLine());
-			    int start = Integer.parseInt(st.nextToken());
-			    int end = Integer.parseInt(st.nextToken());
-			    int cost = Integer.parseInt(st.nextToken());
-			    if(floyd[start][end] > -cost){
-			        floyd[start][end] = -cost;
-			    }
+
+			for(int i=0; i<W; i++) {
+				st = new StringTokenizer(br.readLine());
+				int start = Integer.parseInt(st.nextToken());
+				int end = Integer.parseInt(st.nextToken());
+				int cost = Integer.parseInt(st.nextToken());
+				nodeList.add(new node(start,end,-cost));
 			}
-			for(int k=1; k<N+1; k++){
-			    for(int i=1; i<N+1; i++){
-			        for(int j=1; j<N+1; j++){
-			            if(floyd[i][j] > floyd[i][k] + floyd[k][j]){
-			                floyd[i][j] = floyd[i][k] + floyd[k][j];
-			            }
-			        }
-			    }
-			}
-			String result = "NO";
+			result = "NO";
 			for(int i=1; i<N+1; i++){
-			    if(floyd[i][i] < 0){
-			        result = "YES";
-			    }
+			    bellman(i);
+			    if(result == "YES") break;
 			}
+            
 			System.out.println(result);
 		}
+	}
+	
+	static void bellman(int n){
+	    int dist[] = new int[N+1];
+	    Arrays.fill(dist, Integer.MAX_VALUE);
+	    dist[n] = 0;
+	    for(int i=1; i<=N; i++){
+	        boolean update = false;
+	        for(node curnode : nodeList){
+	            if(dist[curnode.x] == Integer.MAX_VALUE) continue;
+	            if(dist[curnode.y] > dist[curnode.x] + curnode.cost){
+	                dist[curnode.y] = dist[curnode.x] + curnode.cost;
+	                update = true;
+	                if(i==N){
+	                    result = "YES";
+	                    return;
+	                }
+	            }
+	        }
+	        if(update == false) return;
+	    }
 	}
 }
